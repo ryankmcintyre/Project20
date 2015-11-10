723753149bb6dd638da20b2ed1a1b46dc551e901
Ryan McIntyre <ryankmcintyre@gmail.com>
2015-11-09 17:45:02 -0700

    Initial check-in with UI fabric and tab control, partially implemented.

---
+++ ContentMixr/ContentMixrWeb/App/Home/Home.js
@@ -0,0 +1,36 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            // Initialize Office UI Fabric Pivot
+            if ($.fn.Pivot) {
+                $('.ms-Pivot').Pivot();
+            }
+
+            addTabEvent();
+        });
+    };
+
+    function addTabEvent() {
+        $('.ms-Pivot').on('click', '.ms-Pivot-link', function (event) {
+            //event.preventDefault();
+            var clickedTabId = $(this).attr("id");
+            var divToShow = clickedTabId.replace("tab", "");
+
+            app.showNotification("test", "clicked on: " + divToShow);
+        });
+    }
+
+    function displayTab(tabToDisplay) {
+        event.preventDefault();
+        $(this).siblings('.ms-Pivot-link').removeClass('is-selected');
+        $(this).addClass('is-selected');
+    }
+
+})();
