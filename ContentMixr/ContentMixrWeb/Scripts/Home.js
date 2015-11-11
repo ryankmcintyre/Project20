8ccb691a1e0372663a842924c740c03a35fd6e43
Ryan McIntyre <ryankmcintyre@gmail.com>
2015-11-11 14:48:05 -0700

    Finished adding MVC plumbing and bringing code over.

---
+++ ContentMixr/ContentMixrWeb/Scripts/Home.js
@@ -0,0 +1,35 @@
+﻿/// <reference path="../App.js" />
+
+(function () {
+    "use strict";
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            // Initialize Office UI Fabric Controls
+            if ($.fn.Pivot) {
+                $('.ms-Pivot').Pivot();
+            }
+            if ($.fn.SearchBox) {
+                $('.ms-SearchBox').SearchBox();
+            }
+
+            addTabEvent();
+        });
+    };
+
+    function addTabEvent() {
+        $('.ms-Pivot').on('click', '.ms-Pivot-link', function (event) {
+            //event.preventDefault();
+            var clickedTabId = $(this).attr("id");
+            var divToShow = clickedTabId.replace("tab", "");
+            // Hide all divs, then show the one selected
+            $('#Flickr').hide();
+            $('#Code').hide();
+            $('#' + divToShow).show();
+        });
+    }
+
+})();
--- ContentMixr/ContentMixrWeb/Scripts/_references.js
+++ ContentMixr/ContentMixrWeb/Scripts/_references.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="jquery-1.9.1.js" />
+﻿/// <reference path="modernizr-2.6.2.js" />
+/// <reference path="jquery-1.10.2.js" />
 /* Required to correctly initalize Office.js for intellisense */
 /// <reference path="_officeintellisense.js" />
 /* Use offline copy of Office.js for intellisense */
